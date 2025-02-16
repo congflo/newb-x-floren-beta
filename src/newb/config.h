75a00b8e769defccc34f694dfe952c790879fdef
congcq <aiskai2007@gmail.com>
2025-02-16 17:43:59 +0700

    update for iOS

--- src/newb/config.h
+++ src/newb/config.h
@@ -93,7 +93,7 @@
 #define NL_PLANTS_WAVE 0.1895    // [toggle] 0.02 gentle ~ 0.4 violent
 #define NL_LANTERN_WAVE 0.16   // [toggle] 0.05 subtle ~ 0.4 large swing
 #define NL_WAVE_SPEED 2.3      // 0.5 slow wave ~ 5.0 very fast wave
-//#define NL_EXTRA_PLANTS_WAVE // [toggle] !dont use! wave using texture coords (1.21.0 vanilla 2048x1024)
+#define NL_EXTRA_PLANTS_WAVE // [toggle] !dont use! wave using texture coords (1.21.0 vanilla 2048x1024)
 
 /* Water */
 #define NL_WATER_TRANSPARENCY 0.6 // 0.0 transparent ~ 1.0 normal
@@ -118,7 +118,7 @@
 /* Vanilla cloud settings - make sure to remove clouds.png when using this */
 #define NL_CLOUD0_THICKNESS 2.1      // 0.5 slim ~ 8.0 fat
 #define NL_CLOUD0_RAIN_THICKNESS 4.0 // 0.5 slim ~ 8.0 fat
-#define NL_CLOUD0_OPACITY 0.9        // 0.0 invisible ~ 1.0 opaque
+#define NL_CLOUD0_OPACITY 1.        // 0.0 invisible ~ 1.0 opaque
 #define NL_CLOUD0_MULTILAYER         // [toggle] extra cloud layer
 
 /* Soft cloud settings */
@@ -129,13 +129,13 @@
 #define NL_CLOUD1_OPACITY 0.9              // 0.0 invisible ~ 1.0 opaque
 
 /* Rounded cloud Settings */
-#define NL_CLOUD2_THICKNESS 2.56789      // 0.5 slim ~ 5.0 fat
+#define NL_CLOUD2_THICKNESS 2.32      // 0.5 slim ~ 5.0 fat
 #define NL_CLOUD2_RAIN_THICKNESS 3.5 // 0.5 slim ~ 5.0 fat
-#define NL_CLOUD2_STEPS 8            // 3 low quality ~ 16 high quality
+#define NL_CLOUD2_STEPS 16            // 3 low quality ~ 16 high quality
 #define NL_CLOUD2_SCALE 0.022        // 0.003 large ~ 0.3 tiny
 #define NL_CLOUD2_SHAPE 0.95          // 0.0 round ~ 1.0 box
 #define NL_CLOUD2_DENSITY 100.0       // 1.0 blurry ~ 100.0 sharp
-#define NL_CLOUD2_VELOCITY 2.3       // 0.0 static ~ 4.0 very fast
+#define NL_CLOUD2_VELOCITY 1.68       // 0.0 static ~ 4.0 very fast
 //#define NL_CLOUD2_MULTILAYER       // [toggle] extra cloud layer
 
 /* Aurora settings */
@@ -143,11 +143,11 @@
 #define NL_AURORA_VELOCITY 0.03 // 0.0 static ~ 0.3 very fast
 #define NL_AURORA_SCALE 0.02    // 0.002 large ~ 0.4 tiny
 #define NL_AURORA_WIDTH 0.3    // 0.04 thin line ~ 0.4 thick lines
-#define NL_AURORA_COL1 vec3(0.1,1.0,0.0)
-#define NL_AURORA_COL2 vec3(0.1,0.0,1.0)
+#define NL_AURORA_COL1 vec3(0.11, 0.24, 0.43)
+#define NL_AURORA_COL2 vec3(0.11, 0.44, 0.53)
 
 /* Shooting star */
-#define NL_SHOOTING_STAR 2.0        // [toggle] 0.2 dim ~ 1.0 bright
+#define NL_SHOOTING_STAR 10.        // [toggle] 0.2 dim ~ 1.0 bright
 #define NL_SHOOTING_STAR_PERIOD 6.0 // 0.4 fast ~ 12.0 slow (12 secs)
 #define NL_SHOOTING_STAR_DELAY 64.0 // 0.0 no delay ~ 100.0 long time (100 secs)
 
@@ -155,18 +155,18 @@
 #define NL_CHUNK_LOAD_ANIM -10.0 // [toggle] -600.0 fall from top ~ 600.0 rise from bottom
 
 /* Sun/Moon */
-#define NL_SUNMOON_ANGLE 30.0 // [toggle] 0.0 no tilt ~ 90.0 tilt of 90 degrees
+#define NL_SUNMOON_ANGLE 35.0 // [toggle] 0.0 no tilt ~ 90.0 tilt of 90 degrees
 #define NL_SUNMOON_SIZE 0.67     // 0.3 tiny ~ 4.0 massive
 #define NL_SUN_BLOOM
-#define NL_SUN_BLOOM_COL vec3(0.98,0.96,0.98)/5.0
+#define NL_SUN_BLOOM_COL vec3(0.98,0.96,0.98)/5.
 #define NL_MOON_BLOOM
-#define NL_MOON_BLOOM_COL vec3(0.86,0.96,0.98)/2.0
+#define NL_MOON_BLOOM_COL vec3(0.86,0.96,0.98)/2.
 
 /* Fake godrays during sunrise/sunset */
 //#define NL_GODRAY 0.8 // [toggle] 0.1 subtle ~ 0.8 strong
 
 /* Sky reflection */
-//#define NL_GROUND_REFL 0.4       // [toggle] 0.2 slightly reflective ~ 1.0 fully reflect sky 
+#define NL_GROUND_REFL 0.4       // [toggle] 0.2 slightly reflective ~ 1.0 fully reflect sky 
 #define NL_GROUND_RAIN_WETNESS 1.0 // 0.0 no wetness ~ 1.0 fully wet blocks when raining
 #define NL_GROUND_RAIN_PUDDLES 1.0 // 0.0 no puddles ~ 1.0 puddles
 //#define NL_GROUND_AURORA_REFL    // [toggle] aurora reflection on ground
@@ -187,6 +187,26 @@
   Build tool will enable corresponding flags when compiling. 
 */
 
+#ifdef MULTILAYER_CLOUDS
+  #undef NL_CLOUD2_THICKNESS
+  #undef NL_CLOUD2_RAIN_THICKNESS
+  #undef NL_CLOUD2_STEPS
+  #undef NL_CLOUD2_SCALE
+  #undef NL_CLOUD2_SHAPE
+  #undef NL_CLOUD2_DENSITY
+  #undef NL_CLOUD2_VELOCITY
+  #undef NL_CLOUD2_MULTILAYER
+
+  #define NL_CLOUD2_THICKNESS 2.1
+  #define NL_CLOUD2_RAIN_THICKNESS 3.5
+  #define NL_CLOUD2_STEPS 16
+  #define NL_CLOUD2_SCALE 0.067
+  #define NL_CLOUD2_SHAPE 0.89
+  #define NL_CLOUD2_DENSITY 100.0
+  #define NL_CLOUD2_VELOCITY 2.3
+  #define NL_CLOUD2_MULTILAYER
+
+#endif
 
 #ifdef EXTRA_MULTILAYER_CLOUDS
   #undef NL_CLOUD2_THICKNESS
@@ -208,5 +228,6 @@
   #define NL_CLOUD2_MULTILAYER
 
 #endif
+
 
 #endif
