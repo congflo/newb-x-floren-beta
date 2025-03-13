19a742ee20f35df4a37798c456de199c9d2e7d2e
congflo <chucong491@gmail.com>
2025-03-13 22:59:00 +0700

    correct clouds color similar sky color

--- src/newb/functions/clouds.h
+++ src/newb/functions/clouds.h
@@ -134,8 +134,8 @@ vec4 renderClouds(vec2 p, float t, float rain, vec3 horizonCol, vec3 zenithCol, 
 
   vec4 col;
   col.a = a + c*(1.0-a);
-  col.rgb = horizonCol;
-  col.rgb = mix(col.rgb, 0.5*zenithCol, shadow*mix(b, d, c));
+  col.rgb = 0.8 * horizonCol;
+  col.rgb = mix(col.rgb, zenithCol, shadow*mix(b, d, c));
   col.rgb *= 1.0-0.7*rain;
 
   return col;
